0fd223e84ee695fcdca022b7dd952ca87cbc25ea
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-01 03:21:23 -0500

    Provide a solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
-
+    let list = arguments[0];
+    console.log(list);
+    for(i=1; i < arguments.length;i++){
+        console.log(i);
+        for(k=0; k < list.length;){
+            if(list[k]===arguments[i]){
+               const removed = list.splice(k,1);
+            }
+            k++
+            console.log(list);
+        };
+    };
+    return list;
 };
 
 // Do not edit below this line
