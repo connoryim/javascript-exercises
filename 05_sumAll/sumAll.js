cd4616ffacd18e75883e10afd52527dc0993f6d4
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-01 04:09:53 -0500

    Provide a solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(first,second) {
+    let sum =0;
+    if(typeof first !== "number"|| typeof second !== "number"){
+        return "ERROR";
+    }
+    else if(first < second && first>=0){
+        for(i=first;i<=second;i++){
+            sum+=i
+        }
+        return sum;
+    }
+    else if(first > second && second>=0){
+        for(i=second;i<=first;i++){
+            sum+=i
+        }
+        return sum;
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
