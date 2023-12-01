b627a5418b21faec492430e31dd0dfbe625ba437
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-01 01:46:12 -0500

    Provide a solution to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+function repeatString(phrase,num) {
+    let returnString = "";
+    let i = 0;
+    if(num<0){
+        return "ERROR";
+    }
+    while(i<num) {
+        returnString = returnString + phrase;
+        i++;
+        console.log(returnString);
+    }  
+    return returnString;
 };
 
 // Do not edit below this line
