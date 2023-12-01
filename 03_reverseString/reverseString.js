ec9c82d033978111a30aaaab312a365ca3d153c5
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-01 02:25:55 -0500

    Provide a solution to 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    stringArray = string.split("");
+    let reverse = ""
+    let stringSize = string.length;
+    for(i=0; i<stringSize; i++) {
+        let reverseLetter = stringSize - i - 1
+        reverse =reverse + stringArray[reverseLetter];
+        console.log(reverse);
+    };
+    return reverse;
 
 };
 
