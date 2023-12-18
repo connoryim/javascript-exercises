0f402f3134f8d28a7ef95788ea9cbb701b8afddc
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-18 04:26:21 -0500

    Offer a solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    var punctuation = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g
+    var letters = word.toLowerCase().replace(punctuation,"").split("");
+    letters = letters.filter(word =>{
+        return /\S/.test(word);
+    });
+    console.log(letters);
+    for(i = 0; i<= letters.length; i++){
+        var j = letters.length - i -1;
+        if (letters[i]!== letters[j]) {
+            console.log(letters[i]);
+            console.log(letters[j]);
+            return false;            
+        }
+    };
+    return true;
 };
 
 // Do not edit below this line
