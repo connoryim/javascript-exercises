d6da836adcfcbf475ca93bd51d297dbd6f93ef89
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-18 11:21:43 -0500

    Offer solution to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear =new Date().getFullYear() ;
+    const older = people.sort((a,b) =>{
+        if(b.yearOfDeath == undefined){
+            console.log("hello")
+            const lastGuy = a.yearOfDeath - a.yearOfBirth;
+            const nextGuy = currentYear - b.yearOfBirth;
+            console.log(nextGuy);
+            return lastGuy > nextGuy ? -1:1; 
+            
+        } else if (a.yearOfDeath == undefined){
+            console.log("goodbye")
+            const lastGuy = currentYear- a.yearOfBirth;
+            const nextGuy = b.yearOfDeath- b.yearOfBirth;
+            return lastGuy > nextGuy ? -1:1; 
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - a.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    return older[0];
 };
 
 // Do not edit below this line
