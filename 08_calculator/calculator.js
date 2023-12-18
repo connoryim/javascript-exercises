a093ea4d741943954406e695e94ddb98201a71e0
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-18 03:56:25 -0500

    Offer solution to 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+	var total = 0;
+  for(number in numList){
+    total += numList[number];
+  };
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  var total = 1;
+  for(number in numList){
+    total *= numList[number];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  var total = 1;
+  if (a ===0) {
+    return 1;
+  } else {
+    while(a>=1){
+      total *=a
+      a -= 1;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
