2e23ae3fcc91e022df48d4690bb211c27ed6cc3e
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-18 10:25:43 -0500

    Offer solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 1) {
+        return "OOPS";
+    }
+    let fib =[1,1]
+    for(i = 0; i < number; i++){
+        fib[i+2] = fib[i] + fib[i+1];
+    };
+    console.log(fib);
+    return fib[number-1];
 };
 
 // Do not edit below this line
